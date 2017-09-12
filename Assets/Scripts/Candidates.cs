3f4603372fef02ce0136d870d08a5c5521a413cd
Aleksei Waddington <amw694@nyu.edu>
2017-09-11 20:20:27 -0400

    Candidates will now have a name generated for them. File with all the jobs and interships created

--- Assets/Scripts/Candidates.cs
+++ Assets/Scripts/Candidates.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Candidates : MonoBehaviour {
-
+    public SelectCandidate GameManager;
+    public string fullName;
 	// Use this for initialization
 	void Start () {
-		
+        GameManager = FindObjectOfType<SelectCandidate>();
+        if (Random.Range(0, 100) > 50)
+        {
+            fullName = GameManager.generateName(true);
+        }else
+        {
+            fullName = GameManager.generateName(false);
+        }
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/SelectCandidate.cs
+++ Assets/Scripts/SelectCandidate.cs
@@ -12,25 +12,39 @@ using UnityEngine.UI;
 public class SelectCandidate : MonoBehaviour {
 
     public GameObject[] candidates = new GameObject[4], candidatePrefabs;
+    public string[] M_Names_Array, F_Names_Array, lastName_Array;
+    public TextAsset M_Names, F_Names, lastName;
     public int currentDisplayedCandidate = 2, lastDisplayedCandidate = 0;
     public enum GameState { wait, start, fadeOut, moveRight, moveLeft };
     public GameState currentState;
     float lastStateChange = 0.0f, time = 0.0f, alpha;
     public float leftPosition, centerPosition, rightPosition;
-    public GameObject buttonRight, buttonLeft; 
+    public GameObject buttonRight, buttonLeft;
     public Image blackFader;
     bool interactable = true;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         //we make sure the left and right buttons are off
         turnLeftRightOff();
         //set the current state to the starting state
+        if (M_Names != null)
+        {
+            M_Names_Array = M_Names.text.Split(' ');
+        }
+        if (F_Names != null)
+        {
+            F_Names_Array = F_Names.text.Split(' ');
+        }
+        if (lastName != null)
+        {
+            lastName_Array = lastName.text.Split(' ');
+        }
         setCurrentState(GameState.start);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update() {
         //if the player wants to select a candidate
         //once the left mouse button if pressed 
         if (Input.GetKeyDown(KeyCode.Mouse0))
@@ -38,7 +52,7 @@ public class SelectCandidate : MonoBehaviour {
             //we send a racayst in 2d space originating from the mouse position
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
             //we check if it hits anything
-            if(hit && interactable)
+            if (hit && interactable)
             {
                 //save a temp reference of the object that got hit
                 GameObject tempHit = hit.transform.gameObject;
@@ -52,7 +66,7 @@ public class SelectCandidate : MonoBehaviour {
                 //Spawn a set of candidated
                 for (int i = 1; i < candidates.Length; i++)
                 {
-                    GameObject tempCandidate = Instantiate(candidatePrefabs[Random.Range(0, candidatePrefabs.Length - 1)], new Vector3(1000,1,0), Quaternion.identity);
+                    GameObject tempCandidate = Instantiate(candidatePrefabs[Random.Range(0, candidatePrefabs.Length - 1)], new Vector3(1000, 1, 0), Quaternion.identity);
                     candidates[i] = tempCandidate;
                 }
                 //have the middle candidate be in the center of the screen
@@ -61,7 +75,7 @@ public class SelectCandidate : MonoBehaviour {
                 //fade out of black
                 setCurrentState(GameState.fadeOut);
                 time = 0.0f;
-                               
+
                 break;
 
             case GameState.fadeOut:
@@ -97,7 +111,7 @@ public class SelectCandidate : MonoBehaviour {
                 candidates[currentDisplayedCandidate].transform.position = new Vector2(Mathf.SmoothStep(rightPosition, centerPosition, time), candidates[currentDisplayedCandidate].transform.position.y);
 
                 //wait 2 seconds
-                if(getStateElapsed() > 2.0f)
+                if (getStateElapsed() > 2.0f)
                 {
                     //set gamestate to wait
                     setCurrentState(GameState.wait);
@@ -123,7 +137,7 @@ public class SelectCandidate : MonoBehaviour {
                 }
                 break;
         }
-	}
+    }
 
     /// <summary>
     /// Button was clicked to initiate the movement of the candidates to the right.
@@ -135,18 +149,18 @@ public class SelectCandidate : MonoBehaviour {
         //keep an index reference of the candidate currently on screen that needs to be moved
         lastDisplayedCandidate = currentDisplayedCandidate;
         //check if this is the first candidate in the array
-        if(currentDisplayedCandidate == 1)
+        if (currentDisplayedCandidate == 1)
         {
             //then loop around the array
             currentDisplayedCandidate = 3;
-        }else
+        } else
         {
             //otherwise just get the index before it
             currentDisplayedCandidate -= 1;
         }
         //set the state to move right
         setCurrentState(GameState.moveRight);
-        
+
 
     }
 
@@ -163,7 +177,7 @@ public class SelectCandidate : MonoBehaviour {
         {
             //then loop around the array
             currentDisplayedCandidate = 1;
-        }else
+        } else
         {
             //otherwise just get the index after it
             currentDisplayedCandidate += 1;
@@ -210,6 +224,26 @@ public class SelectCandidate : MonoBehaviour {
         buttonLeft.SetActive(false);
         interactable = false;
     }
+    /// <summary>
+    /// generates and returns a full name for the candidate
+    /// </summary>
+    /// <returns></returns>
+    public string generateName(bool gender)
+    {
+        string fullName = "";
+        if (gender)
+        {
+            fullName = F_Names_Array[Random.Range(0, F_Names_Array.Length)];
+            fullName += " " + lastName_Array[Random.Range(0, lastName_Array.Length)];
+        } else
+        {
+            fullName = M_Names_Array[Random.Range(0, M_Names_Array.Length)];
+            fullName += " " + lastName_Array[Random.Range(0, lastName_Array.Length)];
+        }
+
+        return fullName;
+    }
+
 
     
 }
